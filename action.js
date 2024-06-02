f2d8800f7c2abeb07c49c5431b3dd627e6148753
sacreations <supunadithya@hotmail.com>
2024-06-02 07:58:19 +0530

    remove js navigation function

--- action.js
+++ action.js
@@ -12,19 +12,3 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-// set navigation scroll speed
-
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        const offsetTop = targetElement ? targetElement.offsetTop : 0;
-
-        window.scroll({
-            top: offsetTop,
-            behavior: 'smooth'
-        });
-    });
-});
